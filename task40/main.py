85d6c43275e0fa75dfca16ca7159294720df7cec
APavlov <markmarkovkin@gmail.com>
2023-02-27 12:22:07 +0300

    resolved task40 and task42

--- task40/main.py
+++ task40/main.py
@@ -5,12 +5,15 @@
 
 f_path = 'task40/california_housing_train.csv'
 
+# Разбиваем строку и удаляем лишние ковычки
 def separate(text):
     return [key.strip('"') for key in text.strip().split(',')]
 
+# Фильтруем словарь, оставльяя только необходимое
 def filterdict(f, dict_):
     return {i:items for i, items in file_dict.items() if f(items)}
 
+# Среднее значение по полю field
 def average(dict_, field):
     list_ = list(dict_.values())
     return round(sum([a[field] for a in list_]) / len(list_), 4)
@@ -18,20 +21,17 @@ def average(dict_, field):
 def read_csv(path):
     dict_ = dict()
     with open(path) as f:
+        # Вытаскиваем имена полей из первой строки файла
         field_list = separate(f.readline())
-        # print(field_list)
         for i, line in enumerate(f):
             value_list = separate(f.readline())
-            # print(value_list)
+            # наполняем словарь данными из файла, с ключами имен полей
             dict_[i] = {key:float(value_list[a]) for a, key in enumerate(field_list)}
-            # print(dict_)
-            # return
     return dict_
 
 file_dict = read_csv(f_path)
-
 filter_dict = filterdict(lambda x : x['population']<=500, file_dict)
 
-# print(len(file_dict),' =>',len(filter_dict))
 print('средняя стоимость дома : ',average(file_dict,'median_house_value'))
+
 
--- task42/main.py
+++ task42/main.py
@@ -1,3 +1,47 @@
 # Задача 42: Узнать какая максимальная households в зоне минимального значения population
 
 # california_housing_train.csv
+
+f_path = 'task40/california_housing_train.csv'
+
+# Разбиваем строку и удаляем лишние ковычки
+def separate(text):
+    return [key.strip('"') for key in text.strip().split(',')]
+
+# Фильтруем словарь, оставльяя только необходимое
+def filterdict(f, dict_):
+    return {i:items for i, items in file_dict.items() if f(items)}
+
+# Среднее значение по полю field
+def average(dict_, field):
+    list_ = list(dict_.values())
+    return round(sum([a[field] for a in list_]) / len(list_), 4)
+
+def read_csv(path):
+    dict_ = dict()
+    with open(path) as f:
+        # Вытаскиваем имена полей из первой строки файла
+        field_list = separate(f.readline())
+        for i, line in enumerate(f):
+            value_list = separate(f.readline())
+            # наполняем словарь данными из файла, с ключами имен полей
+            dict_[i] = {key:float(value_list[a]) for a, key in enumerate(field_list)}
+    return dict_
+
+file_dict = read_csv(f_path)
+
+sorted_list = sorted(file_dict.values(), key=lambda x: x['population'], reverse=False)
+
+# Перебор отсортированного списка среди минимальных значений 'population'
+# Мксимальный 'households'
+population = next(iter(sorted_list))['population']
+households = next(iter(sorted_list))['households']
+
+for a,items in enumerate(sorted_list):
+    if population < items['population']:
+        break
+    if households < items['households']:
+        households = items['households']
+
+print('Минимальное значение `population`: ',population)
+print('Максимальное значение `households`: ',households)
